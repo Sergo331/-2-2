2a38c889478de78d66a44fdced2d87cbde96741b
Sergo331 <proha331@gmail.com>
2023-04-26 18:50:38 +0300

    задача 2-2
    задача 2-2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace циклы_задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string exitProgram = "";
+                while (exitProgram != "exit")
+
+                {
+                    Console.WriteLine("Программа работает, пока не введете 'exit' ");
+                    exitProgram = Console.ReadLine();
+
+                }
+
+            }
+        }
+    }
+}
